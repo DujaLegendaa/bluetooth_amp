36a5b5aa753142dc8a8e07e1d9b49193fc32e3e4
DujaLegendaa <dorizmeo2@gmail.com>
2022-10-08 21:51:38 +0200

    added quality

--- apps/bluetooth_amp_web/lib/bluetooth_amp_web/file_server.ex
+++ apps/bluetooth_amp_web/lib/bluetooth_amp_web/file_server.ex
@@ -1,10 +1,11 @@
 defmodule BluetoothAmpWeb.B3 do
 
-  def upload(name, file_path) do
+  def upload(name, file_path, quality \\ 100) do
     HTTPoison.post!(
       "#{Application.get_env(:bluetooth_amp_web, :file_server)}/upload", 
       {:multipart, [
         {"name", name},
+        {"quality", quality},
         {:file, file_path}
       ]}
     )
